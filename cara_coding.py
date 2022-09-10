5b4efdd34822438f52dab070064f66cea6180a6d
yamcahai <113223316+yamcahai@users.noreply.github.com>
2022-09-10 10:30:23 +0700

    menambahkan file

---
+++ cara_coding.py
@@ -0,0 +1,54 @@
+
+
+
+def fungsi_python():
+    print("hai ini adalah fungsi")
+
+fungsi_python()
+print("mahrus")
+fungsi_python()
+# ifrgugbng
+# ctrl+/
+fungsi_python()
+# iuhyf8y
+# wfuyfy89yf9
+# 9rutr8ty
+fungsi_python()
+nama = 'mahrus'
+umur = '20'
+print(  nama )
+print('nama saya adalah ' + nama + ' umur saya ' + umur)
+
+
+# bolean
+
+
+
+# list
+# hewan ={0,1,2,3}
+hewan=['ayam', 'kucing', 'kelinci', 'tupai']
+hewanlaut=('cumi', 'udang', 'hiu')
+
+print(hewan[2])
+angka1 = 5
+angka2 = 10
+angka3 = 20
+print("perjumlahan : " + str(angka1 + angka2 + angka3))
+print("pengurangan : " + str( angka1 ))
+print("pembagia : " + str(angka1 / angka2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+nama = imput("masukkan nama anda : ")
+print("ternyata nama anda adalah : " + nama)
